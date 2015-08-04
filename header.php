e0bb77afb65417c0bd3db322b1ff7f070444a021
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-08-04 13:50:00 -0500

    haciendo los enlaces a index.php (footer, header)

---
+++ header.php
@@ -0,0 +1,15 @@
+<!doctype <!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+	<title>Sistema de encuesta</title>
+</head>
+    <nsv id="menu">
+        <ul>
+        	<li><a href="index.php" id="logo">&ndsp;</a></li>
+        	<li><a href="index.php" id="enlace" class="inicio">Inicio</a></li>
+        	<li><a href="verEncuesta.php" id="enlaces">Encuestas</a></li>
+        	<li><a href="votarEncuesta2.php" id="enlaces">&ndsp;</a></li>
+        </ul>
+        <a href="pull">&ndsp;</a>
+    </nsv>
--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<!doctype <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-	<title>Sistema de encuesta</title>
-</head>
-<body>
-
-</body>
-</html>>
+<?php
+include("header.php")
+?>
