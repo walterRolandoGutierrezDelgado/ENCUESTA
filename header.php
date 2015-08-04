e4edbbfa11d5f3cae308bbcaf72edae1332ae3f8
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-08-04 15:10:56 -0500

    corrigiendo algunos errores

--- header.php
+++ header.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
 	<title>Sistema de encuesta</title>
 	<link rel="stylesheet" href="css/StylePrincipal.css">
+
 </head>
-    <nsv id="menu">
+<header>
+	 <nav id="menu">
         <ul>
         	<li><a href="index.php" id="logo">&ndsp;</a></li>
-        	<li><a href="index.php" id="enlace" class="inicio">Inicio</a></li>
+        	<li><a href="index.php" id="enlaces" class="inicio">Inicio</a></li>
         	<li><a href="verEncuesta.php" id="enlaces">Encuestas</a></li>
         	<li><a href="votarEncuesta2.php" id="enlaces">Responder</a></li>
         </ul>
         <a href="pull">&ndsp;</a>
-    </nsv>
+    </nav>
+</header>
