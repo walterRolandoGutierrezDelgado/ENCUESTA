5b9c73671303de0e0c0d0626e3e3f2b5b2999461
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-08-05 15:35:39 -0500

    posibles soluciones

--- php/conexion.php
+++ php/conexion.php
@@ -5,5 +5,4 @@ $pass="root";
 $db="system_encuestas";
 $conexion=mysql_connect($host, $user, $pass) or die("No hay conexion a la base de datos");
 mysql_select_db($db, $conexion) or die ("No existe la base de datos");
-mysql_select_db($db, $conexion) or die ("No existe la base de datos");
 ?>
