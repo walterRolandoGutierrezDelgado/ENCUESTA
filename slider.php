7939ca2e7d2aedb918b0c54e841bdbed1f0e97ca
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-08-04 23:55:44 -0500

    poniendo las etiquetas

--- slider.php
+++ slider.php
@@ -1,9 +1,31 @@
 <div class="container">
     <div id="da-slider" class="da-slider">
-        <div class="daslider">
+        <div class="da-slider">
            <h2>Java</h2>
            <p>programacion en java</p>
-           <div class="da-img"><img arc="images/2.png" alt=""></div>
-        </div>>
-    </div>>
+           <div class="da-img"><img arc="images/2.png" alt="image01"></div>
+        </div>
+
+
+         <div class="da-slider">
+           <h2>CSS</h2>
+           <p>programacion en css</p>
+           <div class="da-img"><img arc="images/3.png" alt="image01"></div>
+        </div>
+
+         <div class="da-slider">
+           <h2>HTML5</h2>
+           <p>programacion en html5</p>
+           <div class="da-img"><img arc="images/1.png" alt="image01"></div>
+        </div>
+         <div class="da-slider">
+           <h2>MYSQL</h2>
+           <p>programacion en MYSQL</p>
+           <div class="da-img"><img arc="images/4.png" alt="image01"></div>
+        </div>
+        <nav class="da-arrows">
+        	<span class="da-arrows-prev"></span>
+        </nav>
+    </div>
+
 </div>>
