ac87b6fe42c2c9e37b02b4c5cf0345243f7ed036
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-08-04 23:45:33 -0500

    le estamos aciendo una referencia a la imagen 2

--- slider.php
+++ slider.php
@@ -1,4 +1,9 @@
 <div class="container">
-    <div id="da-slider">
+    <div id="da-slider" class="da-slider">
+        <div class="daslider">
+           <h2>Java</h2>
+           <p>programacion en java</p>
+           <div class="da-img"><img arc="images/2.png" alt=""></div>
+        </div>>
     </div>>
 </div>>
