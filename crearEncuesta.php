5a4c27d08c4d446744747c097bd68e5ef888b01e
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-08-06 15:50:54 -0500

    diseñando el formulario del login

--- crearEncuesta.php
+++ crearEncuesta.php
@@ -1,3 +1,17 @@
 <?PHP
+include("php/session.php");
  include("header.php");
+
 ?>
+<section id ="form">
+	<form action="crearPreguntas.php" class="contact_form" method="post">
+		<ul>
+			<li>
+				<h2>Craer encuesta</h2>
+			</li>
+			<li>
+				<label for="titulo"></label>
+			</li>
+		</ul>>
+	</form>
+</section>>
