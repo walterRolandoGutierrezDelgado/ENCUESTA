7538ee06ddc9ce08f719b8593696c00726484334
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-08-06 02:34:02 -0500

    estamos llamando a los datos almasenados en la tabla login

--- php/control.php
+++ php/control.php
@@ -5,11 +5,11 @@ $consulta=mysql_query("SELECT * FROM login");
  $filas=mysql_fetch_array($consulta);
 
 
-      $nombre=$filas['nombre'];
-      $usuario=$filas['usuario'];
+      $usuario=$filas['email'];
       $pass=$filas['pass'];
 
 
-   if ($_POST['user']==$usuario && $_POST['pass']==$pass){
-   	
+   if ($email_POST['email']!==$usuario){
+
+
    } 
