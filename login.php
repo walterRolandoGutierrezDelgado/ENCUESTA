b094147b639768163a3d3540336b6b4e063d452a
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-08-05 23:16:08 -0500

    tratando de arreglar el login parese q no hay conexion con la base de datos (en pruebas)

--- login.php
+++ login.php
@@ -5,29 +5,29 @@ include("header.php");
     <form class="contact_form" action="php/control.php" method="post">
          <ul>
             <li>
-            	<h2>Iniciar sesiòn</h2>
+            	<h2>Iniciar sesion</h2>
             </li>
-            <h5>Email</h5>
+            <h5>Emial</h5>
             <li>
             	<input type="email" name="email" placeholder="Email"required/>
             </li>
             <h5>Contraseña</h5>
             <li>
-            	<input type="password" name="password" placedolder="Passwoerd" required>
+            	<input type="password" name="password" placedolder="Password" required>
             </li>
             <?php
               if (isset($_POST["email"])){
               	echo "Email incorrecto"
               } else{
-                  if (isset($_POST["passwoerd"])){
-              	   echo "Password es incorrecto";
+                  if (isset($_POST["password"])){
+              	   echo "Contraseña incorrecto";
               	   }
               }
             ?>
             <li>
             	<input type="submit" class="submit" value="Iniciar">
             </li>
-            <a href="paginas1" href="javascript:window.history.back()">&laquo; Volver atras</a>
+            <a href="paginas1" href="javascript:window.history.back();">&laquo; Volver atras</a>
          </ul>
     	
     </form>
