c7e264bbfa2f4b74ff5ae002a78dcbdf1d00dc09
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-08-06 15:57:05 -0500

    creando el label number mas el titulo

--- crearEncuesta.php
+++ crearEncuesta.php
@@ -11,6 +11,13 @@ include("php/session.php");
 			</li>
 			<li>
 				<label for="titulo">Encuesta:</label>
+				<input type="text" name="litulo" placeholder="Encuesta"
+				required/>
+			</li>
+			<li>
+				<label for="preguntas">N&uacute;mero de preguntas:</label>
+				<input type="number" name="preguntas" placeholder="Encuesta"
+				required/>
 			</li>
 		</ul>>
 	</form>
