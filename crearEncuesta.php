814295e24ea74bb0e8c0b1ba2f9d1fdfca999868
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-10-21 21:06:33 -0500

    aumentando scrips a crearencuesta

--- crearEncuesta.php
+++ crearEncuesta.php
@@ -14,12 +14,24 @@ include("php/session.php");
 				<input type="text" name="litulo" placeholder="Encuesta"
 				required/>
 			</li>
-			
+
+
 			<li>
 				<label for="preguntas">N&uacute;mero de preguntas:</label>
 				<input type="number" name="preguntas" placeholder="Preguntas" min="0" 
 				max="5" required/>
 			</li>
+			 <script>
+        $(function(){
+        	var pul =$('#pull')
+        	menu = $('nav ul');
+        	menuHeight = menu.menu.height();
+        	$(pull).on('click',function(e){
+        		e.preventDefault();
+        		menu.slideTogglr();
+        	});
+        });
+    </script>
 			<li>
 			  <input type="submit" class="submit" value="crear"/>
 			</li>
