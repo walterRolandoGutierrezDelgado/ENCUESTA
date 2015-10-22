16165c72028c17e32ee19cb466cdeaa4b208328b
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-10-21 21:00:52 -0500

    creando nuevos parametros de datos pero con problemas a solucionarlo

--- php/crearPregunta.php
+++ php/crearPregunta.php
@@ -10,8 +10,14 @@ $preguntas = $_POST["preguntas"];
         <h3><?php echo $titulo; ?></h3>
         <table>
         	<?php
-        	
+        	for($i=1; $i<=$preguntas ; $i++){
+
         	? >
+        	<tr>
+        	   <td>preguntas <?php echo $i ;?></td>
+        	   <td><imput name="<?phpecho $i;?>"type="text" size ="50
+        	   "maxlength="50"></td>
+        	</tr>
         </table>>
     </form>
  </section>
