0c4025f35db0a32e9c46384ceec2108ebd7c5252
Proyecto Dircetur andahuaylas <walextasis@hotmail.com>
2015-10-21 19:41:16 -0500

    asignando bariables para q pueda guardar preguntas contactos e informacion

--- php/crearPregunta.php
+++ php/crearPregunta.php
@@ -4,3 +4,15 @@ $titulo = $_POST["titulo"];
 $preguntas = $_POST["preguntas"];
 
  ?>}
+ <sectio id="form">
+    <form action="php/insetarPregunta.php>" 
+    class="contact_form" method="post">
+        <h3><?php echo $titulo; ?></h3>
+        <table>
+        	<?php
+        	
+        	? >
+        </table>>
+    </form>
+ </section>
+
